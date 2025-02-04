2112db6b0c9646e24ea6a0ca05bcc3e24d7d002f
Holdwich <matheusflcastro@hotmail.com>
2025-02-04 11:23:49 -0300

    [REFACTOR] Remoção de valor padrão da DATABASE_URL (Usar .env ou docker)

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,4 +1,4 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:senha123@localhost/DBhbm")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
